cfce6e28ad38f290042f3a40dfe7c37b60a3faed
Ajay More <ajaymore0634@gmail.com>
2025-03-07 15:00:45 +0530

    tailwind-preset-and-set-colors | added tailwind preset and setup colors

---
+++ apps/frontend/src/config/tailwind-preset.js
@@ -0,0 +1,26 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: {
+        DEFAULT: 'var(--color-primary)',
+      },
+      secondary: {
+        DEFAULT: 'var(--color-secondary)',
+        100: '#2dba4e',
+      },
+      white: {
+        DEFAULT: 'var(--color-white)',
+      },
+      black: {
+        DEFAULT: 'var(--color-black)',
+      },
+      blue: {
+        DEFAULT: 'var(--color-blue)',
+        100: '#eefaff',
+      },
+      grey: {
+        DEFAULT: 'var(--color-grey)',
+      },
+    },
+  },
+};
